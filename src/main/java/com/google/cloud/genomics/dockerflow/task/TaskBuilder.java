e510f73161bf8d214320a40f50c7b7804959cf58
Jonathan Bingham <binghamj@google.com>
2017-03-13 15:52:36 -0700

    Doc fixes.

--- src/main/java/com/google/cloud/genomics/dockerflow/task/TaskBuilder.java
+++ src/main/java/com/google/cloud/genomics/dockerflow/task/TaskBuilder.java
@@ -66,16 +66,27 @@ public class TaskBuilder {
 
   TaskBuilder() {}
 
+  /**
+   * Create a new task with a given name.
+   */
   public static TaskBuilder named(String name) {
     return new TaskBuilder(name);
   }
 
+  /**
+   * Create a new task from a YAML workflow definition file on local disk or in Cloud Storage.
+   */
   public static TaskBuilder fromFile(String path) throws IOException, URISyntaxException {
     TaskBuilder tb = new TaskBuilder();
     tb.task = WorkflowFactory.load(path);
     return tb;
   }
 
+  /**
+   * Create a new task by copying an existing task and giving it a new name.
+   * You can then modify the new task. Useful if you have two similar tasks
+   * and don't want to repeat all of the code to define parameters.
+   */
   public static TaskBuilder fromTask(Task t, String name) {
     TaskBuilder tb = new TaskBuilder();
     tb.task = new Workflow(t);
@@ -83,22 +94,36 @@ public class TaskBuilder {
     return tb;
   }
 
+  /**
+   * Create a new task from a YAML task definition file on local disk or in Cloud Storage.
+   * A task definition is what the Pipelines API expects as its payload.
+   */
   public static TaskBuilder fromTaskDefnFile(String path) throws IOException {
     TaskBuilder tb = new TaskBuilder(null);
     tb.task.setDefn(WorkflowFactory.loadDefn(path));
     return tb;
   }
 
+  /**
+   * Set or replace the arguments to the task.
+   */
   public TaskBuilder withArgs(TaskArgs args) {
     task.setArgs(args);
     return this;
   }
 
+  /**
+   * Set or override the task name. 
+   */
   public TaskBuilder name(String name) {
     task.getDefn().setName(name);
     return this;
   }
 
+  /*
+   * Set or replace the Docker image name in Docker hub, Google Container Registry,
+   * quay.io or elsewhere. 
+   */
   public TaskBuilder docker(String imageName) {
     if (task.getDefn().getDocker() == null) {
       task.getDefn().setDocker(new Docker());
@@ -107,10 +132,18 @@ public class TaskBuilder {
     return this;
   }
 
+  /**
+   * Declare an input parameter that will be passed to the task as an environment variable.
+   */
   public TaskBuilder input(String name) {
     return input(name, null);
   }
 
+  /**
+   * Declare an input parameter that will be passed to the task as an environment variable
+   * and assign its default value. The value may be itself a variable, like ${foo} or ${PreviousTask.foo}, in
+   * which case it will be evaluated lazily at runtime before executing the task.
+   */
   public TaskBuilder input(String name, String value) {
     if (task.getDefn().getInputParameters() == null) {
       task.getDefn().setInputParameters(new LinkedHashSet<Param>());
@@ -122,20 +155,53 @@ public class TaskBuilder {
     return this;
   }
 
-  public TaskBuilder inputFromFile(String name, String value) {
-    input(name, value);
+  /**
+   * Declare an input parameter that will be passed to the task as an environment variable,
+   * and assign a default value that will be loaded from a file path. 
+   * The value may be itself a variable, like ${foo}, in
+   * which case it will be evaluated lazily at runtime before executing the task.
+   */
+  public TaskBuilder inputFromFile(String name, String path) {
+    input(name, path);
     task.getArgs().setFromFile(name, true);
     return this;
   }
 
+  /**
+   * Declare an input parameter array that will be passed to the task as an environment variable.
+   * @param name
+   * @param delimiter a parameter "foo" with delimiter " -i " will be expanded in the
+   * shell command, so that "${foo}" becomes "${foo_0} -i ${foo_1} -i ${foo_2}"...
+   * where foo_0, foo_1 and foo_2 are elements in the array
+   */
   public TaskBuilder inputArray(String name, String delimiter) {
     return inputArray(name, delimiter, (String[]) null);
   }
 
+  /**
+   * Declare an input parameter array that will be passed to the task as an environment variable
+   * and assign its default value as a whitespace delimited string.
+   * The value may contain variables, like ${foo} or ${PreviousTask.foo}, in
+   * which case it will be evaluated lazily at runtime before executing the task.
+   * @param name
+   * @param delimiter a parameter "foo" with delimiter " -i " will be expanded in the
+   * shell command, so that "${foo}" becomes "${foo_0} -i ${foo_1} -i ${foo_2}"...
+   * where foo_0, foo_1 and foo_2 are elements in the array
+   */
   public TaskBuilder inputArray(String name, String delimiter, String value) {
     return inputArray(name, delimiter, new String[] {value});
   }
 
+  /**
+   * Declare an input parameter array that will be passed to the task as an environment variable
+   * and assign its default value as an array that will be converted to a whitespace delimited string.
+   * The value may be itself a variable, like ${foo} or ${PreviousTask.foo}, in
+   * which case it will be evaluated lazily at runtime before executing the task.
+   * @param name
+   * @param delimiter a parameter "foo" with delimiter " -i " will be expanded in the
+   * shell command, so that "${foo}" becomes "${foo_0} -i ${foo_1} -i ${foo_2}"...
+   * where foo_0, foo_1 and foo_2 are elements in the array
+   */
   public TaskBuilder inputArray(String name, String delimiter, String[] values) {
     if (task.getDefn().getInputParameters() == null) {
       task.getDefn().setInputParameters(new LinkedHashSet<Param>());
@@ -429,6 +495,9 @@ public class TaskBuilder {
     return this;
   }
 
+  /**
+   * Define the bash command that will be executed in the Docker container.
+   */
   public TaskBuilder script(String script) {
     if (task.getDefn().getDocker() == null) {
       task.getDefn().setDocker(new Docker());
@@ -438,72 +507,131 @@ public class TaskBuilder {
     return this;
   }
 
+  /**
+   * Define the bash command that will be executed in the Docker container,
+   * loading the command from a local or Cloud Storage path.
+   */
   public TaskBuilder scriptFile(String path) throws IOException {
     return script(FileUtils.readAll(path));
   }
 
+  /**
+   * Set the min CPU cores for the VM that will run the script.
+   */
   public TaskBuilder cpu(Integer cores) {
     return cpu(String.valueOf(cores));
   }
 
+  /**
+   * Set the min CPU cores for the VM that will run the script.
+   */
   public TaskBuilder cpu(String cores) {
     task.getDefn().getResources().setMinimumCpuCores(cores);
     return this;
   }
 
+  /**
+   * Set the min RAM GB for the VM that will run the script.
+   */
   public TaskBuilder memory(Number gb) {
     return memory(String.valueOf(gb));
   }
 
+  /**
+   * Set the min RAM GB for the VM that will run the script.
+   */
   public TaskBuilder memory(String gb) {
     task.getDefn().getResources().setMinimumRamGb(gb);
     return this;
   }
 
+  /**
+   * Set the disk size in GB that will be mounted to the VM.
+   */
   public TaskBuilder diskSize(Integer gb) {
     return diskSize(String.valueOf(gb));
   }
 
+  /**
+   * Set the disk size in GB that will be mounted to the VM.
+   */
   public TaskBuilder diskSize(String gb) {
     task.getDefn().getResources().getDisks().get(0).setSizeGb(gb);
     return this;
   }
 
+  /**
+   * Use preemptible VMs. If terminated prematurely, Dockerflow will automatically retry
+   * and then escalate to a standard VM.
+   */
   public TaskBuilder preemptible(boolean b) {
     task.getDefn().getResources().setPreemptible(b);
     return this;
   }
 
+  /**
+   * Launch each VM in one of these zones. Zones can include wildcards, like "us-central*".
+   */
   public TaskBuilder zones(String[] zones) {
     task.getDefn().getResources().setZones(WorkflowFactory.expandZones(zones));
     return this;
   }
 
+  /**
+   * After this task completes, if it was run in multiple shards, gather the
+   * shards together, combining on an input parameter. Eg, if the workflow previously
+   * sharded by input file, gathering globally would lead to only one shard for
+   * subsequent steps in the workflow. 
+   * <p>
+   * Tip: to gather globally, define an input variable with a constant value, like
+   * "pipeline_run". Then gather by that value. Because the value is the same in all
+   * shards, the workflow will reduce down to a single task.
+   */
   public TaskBuilder gatherBy(String inputName) {
     task.setGatherBy(inputName);
     return this;
   }
 
+  /**
+   * After this task begins, scatter into multiple shards, based on multiple values
+   * of an input parameter. Eg, if the workflow has an input file named "infile"
+   * scattering by the input file will lead to one task shard per input file.
+   * The value of "infile" must be a newline-delimited string.
+   */
   public TaskBuilder scatterBy(String inputName) {
     task.setScatterBy(inputName);
     return this;
   }
 
+  /**
+   * An optional user-provided ID that will be used to tag tasks.
+   */
   public TaskBuilder clientId(String id) {
     task.getArgs().setClientId(id);
     return this;
   }
 
+  /**
+   * An optional service account email. VMs will run as this service account.
+   */
   public TaskBuilder serviceAccountEmail(String email) {
     task.getArgs().getServiceAccount().setEmail(email);
     return this;
   }
 
+  /**
+   * An optional list service account scopes. VMs will run with these scopes.
+   */
   public TaskBuilder serviceAccountScopes(List<String> scopes) {
     task.getArgs().getServiceAccount().setScopes(scopes);
     return this;
   }
 
+  /**
+   * The cloud project id to run under. All costs will be billed to this account.
+   * The running user must have authenticated against this project using gcloud before running
+   * Dockerflow from the command-line.
+   */
   public TaskBuilder project(String id) {
     task.getDefn().setProjectId(id);
     task.getArgs().setProjectId(id);
@@ -520,11 +648,20 @@ public class TaskBuilder {
     return this;
   }
 
+  /**
+   * Run the workflow in test mode, as a dry run. No API calls will be made. The HTTP payload
+   * of the API calls will be output to stdout. Often when running in test mode, you'll want
+   * to set the Dataflow runner to DirectPipelineRunner using the dockerflow --runner flag
+   * on the command-line.
+   */
   public TaskBuilder testing(boolean b) {
     ((WorkflowArgs)task.getArgs()).setTesting(b);
     return this;
   }
 
+  /**
+   * Return the task object. Call this after fully configuring the task.
+   */
   public Workflow build() {
     return task;
   }
@@ -551,19 +688,38 @@ public class TaskBuilder {
     return graph(o);
   }
 
+  /**
+   * When using the builder to create a workflow rather than an individual task,
+   * you can explicitly define the directed acyclic graph of workflow steps.
+   * Examples: taskA, taskB, taskC.
+   */
   public TaskBuilder graph(Object... graph) {
     return graph(Arrays.asList(graph));
   }
 
+  /**
+   * When using the builder to create a workflow rather than an individual task,
+   * you can explicitly define the directed acyclic graph of workflow steps.
+   */
   public TaskBuilder graph(List<Object> graph) {
     task.setGraph(graph);
     return this;
   }
 
+  /**
+   * When using the builder to create a workflow rather than an individual task,
+   * you can explicitly define the directed acyclic graph of workflow steps.
+   * This method defines a linear sequence of steps.
+   */
   public TaskBuilder steps(Task... tasks) {
     return steps(Arrays.asList(tasks));
   }
 
+  /**
+   * When using the builder to create a workflow rather than an individual task,
+   * you can explicitly define the directed acyclic graph of workflow steps.
+   * This method defines a linear sequence of steps.
+   */
   public TaskBuilder steps(List<Task> tasks) {
     List<Workflow> w = new ArrayList<Workflow>();
     for (Task t : tasks) {
@@ -573,6 +729,12 @@ public class TaskBuilder {
     return this;
   }
 
+  /**
+   * When using the builder to create a workflow rather than an individual task,
+   * you can explicitly define the directed acyclic graph of workflow steps.
+   * Examples: "Steps.of(taskA, taskB, taskC)";
+   * "Steps.of(taskA, Branch.of(taskB, taskC))"
+   */
   public TaskBuilder steps(Steps steps) {
     task.setSteps(steps);
     return this;
@@ -584,6 +746,9 @@ public class TaskBuilder {
     return this;
   }
 
+  /**
+   * Initialize the workflow based on command-line flags passed to Dockerflow.
+   */
   public TaskBuilder args(String[] args) throws IOException {
     task.setArgs(WorkflowFactory.createArgs(args));
     return this;
